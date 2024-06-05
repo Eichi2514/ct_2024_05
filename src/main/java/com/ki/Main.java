2f769feff6663402bbc4d3ce349697a8671506c9
Eichi2514 <picon1317@gmail.com>
2024-06-05 09:27:55 +0900

    프로젝트 세팅

--- src/main/java/com/ki/Main.java
+++ src/main/java/com/ki/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++)
-            System.out.println("i = " + i);
+        for (int a = 1; a <= 5; a++)
+            System.out.println("a = " + a);
     }
 }
