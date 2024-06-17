d57732f57b451c2da6706af1e71756009e52933e
Eichi2514 <picon1317@gmail.com>
2024-06-17 09:26:26 +0900

    p120808

--- src/main/java/com/ki/level0/p120808/Main.java
+++ src/main/java/com/ki/level0/p120808/Main.java
@@ -2,23 +2,78 @@ package com.ki.level0.p120808;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Solution.solution(1, 2, 3, 4));
+        //        int 분자1 = 1;
+//        int 분모1 = 2;
+//        int 분자2 = 1;
+//        int 분모2 = 3;
+//
+//        int[] a = new int[2];
+//        int 최소공배수;
+//        int 큰분모;
+//        int 작은분모;
+//
+//        if (분모1 < 분모2) {
+//            큰분모 = 분모2;
+//            작은분모 = 분모1;
+//            int 작은분모2 = 1;
+//            for (int N = 1; N <= 10; N++) {
+//                for (int n = 1; n <= 10; n++) {
+//                    if ((작은분모2 * n) != (큰분모 * N)) {
+//                    } else if ((작은분모2 * n) == (큰분모 * N)) {
+//                        최소공배수 = 작은분모2*n;
+//                        a[0] = ((최소공배수 / 분모1) * 분자1) + ((최소공배수 / 분모2) * 분자2);
+//                        a[1] = 최소공배수;
+//                    }
+//                    System.out.println(작은분모2 * n);
+//                    System.out.println(큰분모 * N);
+//                    System.out.println(a[0]);
+//                }
+//            }
     }
 }
 
 
-
 class Solution {
-//    int* solution(int numer1, int denom1, int numer2, int denom2) {
-//        // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
-//        int* answer = (int*)malloc(1);
-//        return answer;
-//    }
-    public static int solution(int a, int b, int c, int d) {
+    public int[] solution(int 분자1, int 분모1, int 분자2, int 분모2) {
+        int[] a = new int[2];
+        int 최소공배수;
+        int 큰분모;
+        int 작은분모;
 
-        for (int B = 0; B <= 1000; B++) {
-            B = B + b;
+        if (분모1 < 분모2) {
+            큰분모 = 분모2;
+            작은분모 = 분모1;
+            int 작은분모2 = 1;
+            for (int N = 1; N <= 1000; N++) {
+                for (int n = 1; n <= 1000; n++) {
+                    if ((작은분모2 * n) != (큰분모 * N)) {
+                    } else if ((작은분모2 * n) != (큰분모 * N)) {
+                        최소공배수 = 작은분모2 * n;
+                        a[0] = ((최소공배수 / 분모1) * 분자1) + ((최소공배수 / 분모2) * 분자2);
+                        a[1] = 최소공배수;
+                    }
+                }
+            }
+        } else if (분모2 < 분모1) {
+            큰분모 = 분모1;
+            작은분모 = 분모2;
+            int 작은분모2 = 1;
+            for (int N = 1; N <= 1000; N++) {
+                for (int n = 1; n <= 1000; n++) {
+                    if ((작은분모2 * n) != (큰분모 * N)) {
+                    } else if ((작은분모2 * n) != (큰분모 * N)) {
+                        최소공배수 = 작은분모2 * n;
+                        a[0] = ((최소공배수 / 분모1) * 분자1) + ((최소공배수 / 분모2) * 분자2);
+                        a[1] = 최소공배수;
+                    }
+                }
+            }
+        } else {
+            최소공배수 = 분모1;
+            a[0] = ((최소공배수 / 분모1) * 분자1) + ((최소공배수 / 분모2) * 분자2);
+            a[1] = 최소공배수;
         }
-        return 1+", "+2;
+        int[] ints = {a[0], a[1]};
+        return ints;
     }
 }
